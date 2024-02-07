8055864bddc2f7ee1290db33fb018944331c5074
Hammad Khan <khanhammad9608@gmail.com>
2024-02-07 16:05:08 +0530

    update category table

---
+++ src/components/inputs/ColumnFilter/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ColumnFilter = ({ column }) => {
+    const { filterValue, setFilter } = column;
+
+    return (
+        <input
+            value={filterValue || ""}
+            onChange={(e) => setFilter(e.target.value)}
+            placeholder={`Filter ${column.Header}`}
+        />
+    );
+};
+
+export default ColumnFilter;
