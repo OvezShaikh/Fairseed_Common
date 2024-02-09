7a58c9c6a4cdb97d898095fa562c1e73e10bd04e
Rehaan Dafedar <rehan@gmail.com>
2024-02-09 16:10:04 +0530

    Create Campagin Classname Fixed

--- src/pages/AdminPanel/AdminPage.jsx
+++ src/pages/AdminPanel/AdminPage.jsx
@@ -8,6 +8,7 @@ import Donation from "../../components/AdminPanelPages/Donation/Index";
 import Categories from "../../components/AdminPanelPages/Categories/Index";
 import Pages from "../../components/AdminPanelPages/Pages/Index"
 import AddNew from "../../components/AdminPanelPages/Categories/AddNew"
+import ReportedCauses from "../../components/AdminPanelPages/ReportedCauses/index";
 
 function AdminPage() {
   return (
@@ -84,7 +85,7 @@ function AdminPage() {
         path="/Reported-Cause"
         element={
           <AdminLayout>
-            <h1>Reported-Cause</h1>
+            <ReportedCauses/>
           </AdminLayout>
         }
       />
