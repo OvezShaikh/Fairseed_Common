abe240e356be1e02c4b2e803e01beb3693e37626
Hammad Khan <khanhammad9608@gmail.com>
2024-02-19 15:35:37 +0530

    Status Update

--- src/components/AdminPanelPages/Campaign/Campaign.jsx
+++ src/components/AdminPanelPages/Campaign/Campaign.jsx
@@ -1,63 +1,86 @@
-import React from 'react'
-import ReactTable from '../../Table/index'
+import React from 'react';
+import ReactTable from '../../Table/index';
 import { useState } from 'react';
 import { Button, Checkbox } from '@mui/material';
 import IndeterminateCheckbox from '../../Table/IndeterminateCheckbox';
 import { LocationConfigurationDialog } from '../../admin-console/LocationConfigurationDialog';
-import Columnfilter from '../../Table/Columnfilter'
+import Columnfilter from '../../Table/Columnfilter';
+import { GoDotFill } from "react-icons/go";
 
 const Campaign = () => {
   const [selectedRowID, setSelectedRowID] = useState(null);
+
   const getStatusCellStyle = (status) => {
-    // Define styles for different statuses
-    if (status === 'pending') {
-      return { color: 'red' }; // Set color to red for pending status
-    } else if (status === 'active') {
-      return { color: 'green' }; // Set color to green for active status
+    console.log('Status:', status);
+    if (status === 'Pending') {
+      return {
+        background: '#EBF0ED',
+        color: '#717171'
+      };
+    } else if (status === 'Active') {
+      return {
+        background: '#ECFDF3  ',
+
+        color: '#037847',
+      };
     }
-    return {}; // Default style
+    return {
+      color: 'gray'
+    };
   };
+  // const Status = ({ values }) => {
+  //   // Loop through the array and create a badge-like component instead of a comma-separated string
+  //   return (
+  //     <>
+  //       {values.map((Status, idx) => {
+  //         return (
+  //           <span key={idx} className="badge">
+  //             {Status}
+  //           </span>
+  //         );
+  //       })}
+  //     </>
+  //   );
+  // };
+
   const StatusCell = ({ value }) => (
-    <span style={getStatusCellStyle(value)}>{value}</span>
+    <div className=' flex justify-center gap-1  items-center w-[100px] h-[25px] rounded-3xl' style={getStatusCellStyle(value)}>
+      <span className='' style={getStatusCellStyle(value)}><GoDotFill /></span>
+      <span className='' style={getStatusCellStyle(value)}>{value}</span>
+    </div>
   );
+
   const columns = React.useMemo(
     () => [
       {
-        Header: "ID",
-        accessor: "id",
+        Header: 'ID',
+        accessor: 'id',
         sortable: false,
       },
       {
-        Header: "Name",
-        accessor: "title",
+        Header: 'Name',
+        accessor: 'title',
         sortable: false,
-
-
       },
       {
-        Header: "User",
-        accessor: "user.username",
+        Header: 'User',
+        accessor: 'user.username',
         sortable: false,
-
-
       },
       {
-        Header: "Email",
-        accessor: "user.email",
+        Header: 'Email',
+        accessor: 'user.email',
         sortable: false,
-
       },
       {
-        Header: "Mobile",
-        accessor: "user.mobile_number",
+        Header: 'Mobile',
+        accessor: 'user.mobile_number',
         sortable: false,
-
       },
       {
-        Header: "Goal",
-        accessor: "goal_amount",
+        Header: 'Goal',
+        accessor: 'goal_amount',
         sortable: false,
-
       },
       {
         Header: 'Status',
@@ -65,16 +88,17 @@ const Campaign = () => {
         sortable: false,
         Cell: StatusCell,
 
+
       },
       {
-        Header: "Date",
-        accessor: "end_date",
+        Header: 'Date',
+        accessor: 'end_date',
         sortable: false,
       },
-
     ],
     []
   );
+
   return (
     <div>
       <ReactTable
@@ -82,16 +106,15 @@ const Campaign = () => {
         columns={columns}
         showFilter
         manualPagination
-        title={"Campaign"}
+        title={'Campaign'}
         checkboxComponent={IndeterminateCheckbox}
-        url="/admin-dashboard/campaign"
+        url='/admin-dashboard/campaign'
         extraQuery={{ inactive: true }}
         addButton={<LocationConfigurationDialog />}
-        // addButton={<Button>HElloooooo</Button>}
         selectedRowID={selectedRowID}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Campaign
+export default Campaign;
