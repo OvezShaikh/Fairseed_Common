3b220cf9ca246b917282126e101f4bdda20a4cb9
mohammedsanaved <mosanaved@gmail.com>
2024-02-26 13:49:20 +0530

    dashboard changes except styling

--- src/components/AdminPanelPages/Dashboard/components/MembersByCountries.jsx
+++ src/components/AdminPanelPages/Dashboard/components/MembersByCountries.jsx
@@ -1,27 +1,18 @@
-import React, { useState } from 'react'
+
 import {
   MapContainer,
   TileLayer,
-  Marker,
-  Popup,
-  useMap,
-  useMapEvents,
 } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MembersByCountries = () => {
-  const [mapPosition, setMapPosition] = useState([40, 0]);
   return (
-    <div className='p-7 border border-black rounded-lg'>
-      <MapContainer
-        center={mapPosition}
-        zoom={6}
-        // scrollWheelZoom={true}
-        // className={"h-[100%]"}
-      >
+    <div className='p-7 '>
+      <MapContainer center={[48.8566, 2.3522]} zoom={2}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       </MapContainer>
     </div>
   )
