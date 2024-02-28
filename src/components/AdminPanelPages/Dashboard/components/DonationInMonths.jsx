12e7434df4445341783557dbcc459d08dbb0975f
mohammedsanaved <mosanaved@gmail.com>
2024-02-28 18:15:45 +0530

    changes in DonationMonths.jsx

--- src/components/AdminPanelPages/Dashboard/components/DonationInMonths.jsx
+++ src/components/AdminPanelPages/Dashboard/components/DonationInMonths.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
-import { Bar } from 'react-chartjs-2'
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
 
-const DonationInMonths = ({userData}) => {
+const DonationInMonths = ({ userData }) => {
   return (
     <div>
-      <p className={"mb-3 text-lg font-semibold"}>Donations in months (lacs): </p>
-      <div className='p-7 shadow-md rounded-lg'>
-      <Bar data={userData} />
+      <p className={"mb-3 text-lg font-semibold"}>
+        Donations in months (lacs):{" "}
+      </p>
+      <div className="p-7 shadow-md rounded-lg">
+        <Bar data={userData} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DonationInMonths
+export default DonationInMonths;
