7f8e627bb89bd65dd885f780ee01c0d04a5b6de1
Zaid Shaikh <zackyshaikh99@gmail.com>
2024-02-28 19:45:29 +0530

    resolved cause edit bug

--- src/components/layout/CropAddImage/Index.jsx
+++ src/components/layout/CropAddImage/Index.jsx
@@ -34,7 +34,7 @@ function ImageBackgroundWithDeleteButton({ imgUrl, onDelete, setDataUrl }) {
       className="max-w-[500px] w-full  min-h-[333px]"
       style={backgroundStyle}
     >
-        <img src={imgUrl} alt="" />
+       
       {imgUrl ? (
         <>
           <PrimaryButton
