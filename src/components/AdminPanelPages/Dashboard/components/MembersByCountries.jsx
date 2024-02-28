7a22d0c82cf19c6629c055bbe0568bd79914ac67
mohammedsanaved <mosanaved@gmail.com>
2024-02-28 16:41:31 +0530

    changes in Map Marker

--- src/components/AdminPanelPages/Dashboard/components/MembersByCountries.jsx
+++ src/components/AdminPanelPages/Dashboard/components/MembersByCountries.jsx
@@ -1,16 +1,15 @@
 import { VectorMap } from "@react-jvectormap/core";
 import { worldMill } from "@react-jvectormap/world";
 import React from "react";
+import { country } from "./constant/countries";
 
 const MembersByCountries = () => {
   return (
     <>
       {/* <div className="rounded-lg shadow-lg"> */}
 
-    
-    <div>
-      <p className={"mb-3 text-lg font-semibold"}>Members by Countries: </p>
-
+      <div>
+        <p className={"mb-3 text-lg font-semibold"}>Members by Countries: </p>
         <VectorMap
           map={worldMill}
           containerStyle={{
@@ -18,19 +17,50 @@ const MembersByCountries = () => {
             height: "70%",
           }}
           series={{
-            regionStyle: {
-              initial: {
-                fill: "#ff5566",
-                stroke: "#676767",
-                strokeWidth: 2.5,
-                fillOpacity: 1,
+            regions: [
+              {
+                // scale: colorScale,
+                values: country.map((name) => name.user_count),
+                // min: 0,
+                // max: 100,
               },
-            },
+            ],
           }}
-          backgroundColor="#a3a3a3"
-          />
-          </div>
-          {/* </div> */}
+          labels={country.map((name) => name.user_count)}
+          // series={{
+          //   regions: [
+          //     {
+          //       values: country,
+          //     },
+          //   ],
+          // }}
+          backgroundColor="#ffffff"
+          onRegionTipShow={function reginalTip(event, label, code) {
+            return label.html(`
+                    <div style="background-color: black; border-radius: 6px; min-height: 50px; width: 125px; color: white"; padding-left: 10px>
+                      <p>
+                      <b>
+                      ${label.html()}
+                      </b>
+                      </p>
+                      <p>
+                      ${country.map((name) => name.user_count)}
+                      </p>
+                      </div>`);
+          }}
+          onMarkerTipShow={function markerTip(event, label, code) {
+            return label.html(`
+                    <div style="background-color: white; border-radius: 6px; min-height: 50px; width: 125px; color: black !important; padding-left: 10px>
+                      <p style="color: black !important;">
+                      <b>
+                      ${label.html()}
+                      </b>
+                      </p>
+                      </div>`);
+          }}
+        />
+      </div>
+      {/* </div> */}
     </>
   );
 };
