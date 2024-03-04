1753845e494c9c1289502430f1437a1ca3ba4cc0
Ovez Shaikh <ovezshaikh13052001@gmail.com>
2024-03-04 13:33:24 +0530

    added navbar search button design

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -20,6 +20,8 @@ import UserSignUp_02 from "../../pages/login/Sign_Up/Index";
 import { Link, NavLink } from "react-router-dom";
 import ProfileAvatar from "../../pages/login/ProfileAvatar";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
 
 const styleButton = {
   color: 'red'
@@ -92,7 +94,7 @@ const HowItWorks = [
     name: "How-It-Works",
     href: "/How-It-Works",
   },
- 
+
 
 ];
 
@@ -101,9 +103,23 @@ function classNames(...classes) {
 }
 
 
+
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
- 
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${searchTerm}`); 
+  };
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+
+
+
   // const [isClicked, setIsClicked] = useState(false);
 
   // const handleButtonClick = () => {
@@ -146,7 +162,7 @@ export default function Example() {
           <Popover.Group className="max-nav:hidden lg:flex lg:gap-x-12">
             <Popover className="relative mt-1" >
               <Popover.Button className="flex pt-2 nav_button items-center gap-x-1 text-[18px] font-medium font-[satoshi]  text-[#40444C]"
-                onclick="this.style.backgroundColor = (this.style.backgroundColor === '#40444C') ? 'blue' : '#40444C';"
+                onClick="this.style.backgroundColor = (this.style.backgroundColor === '#40444C') ? 'blue' : '#40444C';"
               // style={buttonStyles}
               // onClick={handleButtonClick}
               >
@@ -349,7 +365,7 @@ export default function Example() {
             </Popover>
             {/* Fourth button */}
 
-            
+
             <button
               className="font-[satoshi] text-[18px] font-medium text-[#40444C]"
             ><Link to="/Home/How-It-Works">How it Works</Link></button>
@@ -380,23 +396,21 @@ export default function Example() {
             }
 
 
-            <div className="flex space-x-8">
-              <button className=" text-black bg-transparent rounded-full ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
+            <div className="flex space-x-8 mt-2">
+              <form onSubmit={handleSearch} className="relative mx-auto flex">
+                <input
+                  type="search"
+                  value={searchTerm}
+                  onChange={handleInputChange}
+                  className="text-xs peer cursor-pointer relative mt-2 z-10 h-8 w-10 rounded-lg border bg-transparent  pr-8 outline-none focus:rounded-r-none focus:w-full focus:cursor-text focus:border-taupeGray focus:px-3"
+                  placeholder="Typing..."
+                />
+                <button type="submit" className="absolute top-0 mt-2 right-0  bottom-0 my-auto h-8 w-10 px-3 bg-transparent rounded-lg peer-focus:relative peer-focus:rounded-l-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                    <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+                  </svg>
+                </button>
+              </form>
               {
                 (localStorage.getItem('token')) ?
                   (<ProfileAvatar />)
